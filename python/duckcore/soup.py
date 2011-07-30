42190f06417b7765f0f9637cb33805b75d638e7e
Eric <esoros1@kent.edu>
2011-07-30 14:13:49 -0400

    added download links

--- python/duckcore/soup.py
+++ python/duckcore/soup.py
@@ -8,22 +8,26 @@ class Post:
 	def __init__(self,post):
 		# s is a string which contains Artist - Album/song (year)
 		#parse the string
-		s = str(post.find('a', { 'href' : re.compile(r'^h') }).string)
-
+		artistAlbumDate = str(post.find('a', { 'href' : re.compile(r'^h') }).string)
+		downloadLinkTags = post.findAll('a', {'href' : re.compile(r'^http://www.mediafire.com') })
 		#I hate regular expressions
 		self.artist = "ARTIST ERROR"
 		self.album = "ALBUM ERROR"
 		self.date = "DATE ERROR"
 
-		match = re.search(r'(.+)-(.+)\s\(?([\w\d-]+)\)?',s)
+		match = re.search(r'(.+)-(.+)\s\(?([\w\d-]+)\)?',artistAlbumDate)
 		if match:
 			self.artist =  match.group(1).strip() 
 			self.album = match.group(2).strip() 
 			self.date = match.group(3).strip() 
 
+		self.downloadLinks = []
+		for link in downloadLinkTags:
+			self.downloadLinks.append(link.attrs[0][1])
+		
+
 #read in the site, and send it into the html parser
 site = urllib.urlopen(r'http://www.duckcore.blogspot.com')
-out = open("out.txt",'w')
 soup = BeautifulSoup(site)
 print "DONE FETCHING SITE"
 
@@ -32,10 +36,19 @@ print "DONE FETCHING SITE"
 posts = []
 
 #fill posts
-for incident in soup.findAll('div', { 'class':'post-outer'}):
-	posts.append(incident.contents[1])
 
-print posts[1]
+for incident in soup.findAll('div', { 'class':'post-outer'}):
+	posts.append(Post(incident.contents[1]))
+
+for post in posts:
+	print post.artist
+	print post.album
+	print post.date
+	print post.downloadLinks
+	print 
+
+
+
 
 
 
